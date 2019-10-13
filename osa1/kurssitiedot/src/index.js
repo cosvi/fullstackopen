70643f760d701fc0d82caa84afd7bd36dbb644ec
Jussi Aaltonen <jussi.a.aaltonen@helsinki.fi>
2019-10-13 17:16:35 +0300

    osa1 1.3

---
+++ osa1/kurssitiedot/src/index.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    const course = 'Half Stack application development'
+    const part1 = {
+        name: 'Fundamentals of React',
+        exercises: 10
+    }
+    const part2 = {
+        name: 'Using props to pass data',
+        exercises: 7
+    }
+    const part3 = {
+        name: 'State of a component',
+        exercises: 14
+    }
+
+    return (
+        <div>
+            <Header text={course} />
+            <Content part1={part1} 
+                part2={part2}
+                part3={part3} />
+            <Total exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises} />
+        </div>
+    )
+}
+
+const Header = (props) => {
+    return (
+        <h1>{props.text}</h1>        
+    )
+}
+
+const Part = (props) => {
+    return (
+        <p>{props.part.name} : {props.part.exercises} exercises</p>
+    )
+}
+
+
+const Content = (props) => {
+    return (
+        <>
+            <Part part = {props.part1} />
+            <Part part = {props.part2} />
+            <Part part = {props.part3} />
+        </>
+    )
+}
+
+const Total = (props) => {
+    return (
+        <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+    )
+}
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
