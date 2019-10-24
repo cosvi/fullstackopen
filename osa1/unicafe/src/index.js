0d2be72280f9a4a91ce07de16f4d9702271f856e
Jussi Aaltonen <jussi.a.aaltonen@helsinki.fi>
2019-10-24 20:22:38 +0300

    osa1 1.11 unicafe step6

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 
 const Statistic = ({text, value}) => {
     return (
-        <>{text}: {value}</>
+        <tr>
+            <td>{text}</td>
+            <td>{value}</td>
+        </tr>
     )
 }
 
-const Statistics = ({good, neutral, bad, all}) => {
+const Statistics = ({good, neutral, bad, all, average, positive}) => {
     if (all === 0) {
         return (
             <>
@@ -18,14 +21,16 @@ const Statistics = ({good, neutral, bad, all}) => {
 
     return (
         <>
-            <p>
-                <Statistic text="good" value={good} /><br />
-                <Statistic text="neutral" value={neutral} /><br />
-                <Statistic text="bad" value={bad} /><br />
-                <Statistic text="all" value={all} /><br />
-                <Statistic text="average" value={(good - bad) / all} /><br />
-                <Statistic text="positive" value={100 * good / all} /> % <br />
-            </p>
+            <table>
+                <tbody>
+                <Statistic text="good" value={good} />
+                <Statistic text="neutral" value={neutral} />
+                <Statistic text="bad" value={bad} />
+                <Statistic text="all" value={all} />
+                <Statistic text="average" value={average} />
+                <Statistic text="positive" value={positive} />
+                </tbody>
+            </table>
         </>
     )
 
@@ -44,6 +49,8 @@ const App = () => {
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
     const all = good + neutral + bad
+    const average = (good-bad) / all
+    const positive = (100 * good / all) + '%'
 
     const increaseGood = () => {
         setGood(good + 1)
@@ -64,7 +71,7 @@ const App = () => {
             <Button setter={increaseNeutral} text="neutral" />
             <Button setter={increaseBad} text="bad" />
             <h1>statistics</h1>
-            <Statistics good={good} neutral={neutral} bad={bad} all={all} />
+            <Statistics good={good} neutral={neutral} bad={bad} all={all} average={average} positive={positive} />
         </div>
     )
 }
